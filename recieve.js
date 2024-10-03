29d063a485b6bca4f596459ee8ba7fac7f897ebc
pl513 <pizzaland513@gmail.com>
2024-10-03 18:42:23 -0400

    Update recieve.js

--- recieve.js
+++ recieve.js
@@ -1,13 +1,2 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.post('/pl513', (req, res) => {
-    console.log(req.body);  // handle the received data
-    res.send('Data received');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// JavaScript test
+console.log("JavaScript is running.")
